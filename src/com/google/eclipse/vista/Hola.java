9255dbd1e49559aebfa52d5bdb50ca7eae533045
Jesús Mendoza <jmenhid2107@g.educaand.es>
2024-10-29 10:43:26 +0100

    cambios en master hola1

--- src/com/google/eclipse/vista/Hola.java
+++ src/com/google/eclipse/vista/Hola.java
@@ -8,7 +8,7 @@ public class Hola {
 		System.out.println("Holaaaaaaaaa!!!!!!");
 
 		System.out.println("Hola!!!!!!");
-		System.out.println("Hola!!!!!!");
+		System.out.println("Hola1!!!!!!");
 
 	}
 
