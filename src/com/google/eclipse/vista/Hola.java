e27ad27d11183cfe0e0171a234c5964a8c5f742c
Jesús Mendoza <jmenhid2107@g.educaand.es>
2024-11-05 09:48:37 +0100

    cambios

--- src/com/google/eclipse/vista/Hola.java
+++ src/com/google/eclipse/vista/Hola.java
@@ -3,7 +3,6 @@ package com.google.eclipse.vista;
 public class Hola {
 
 	public static void main(String[] args) {
-
 		System.out.println("Holaaaaaaaaa!!!!!!");
 		System.out.println("Holaaaaaaaaa!!!!!!");
 
